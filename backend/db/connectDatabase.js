d8b5299cf5a39d764e7ba7f1b0720fea18c73a43
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-09-03 03:34:42 +0530

    solved the bugs

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected' + conn.connection.host);
     } catch (error) {
         console.error('Error in Database Connection : '+ error.message);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDatabase.js";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import problemRoutes from "./routes/problem.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/problems", problemRoutes);
