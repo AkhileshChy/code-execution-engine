8eea0042462514c73f758ef10a6c522a3cefbbea
Akhilesh <choudharyakhilesh2003@gmail.com>
2024-09-03 14:12:18 +0530

    getProblem routes created

--- backend/controllers/problem.controller.js
+++ backend/controllers/problem.controller.js
@@ -7,8 +7,8 @@ export const createProblem = async (req, res) => {
         if (existingProblem) {
             return res.status(400).json({ success: false, message: "Problem title already exists" });
         }
-        const lastProblem = await Problem.findOne({}).sort({ problemNumber: -1 }).exec();
-        const newProblemNumber = lastProblem ? lastProblem + 1 : 1;
+        const lastProblem = await Problem.findOne({}).sort({ problemNumber: -1 });
+        const newProblemNumber = lastProblem ? lastProblem.problemNumber + 1 : 1;
         const newProblem = new Problem({
             problemNumber: newProblemNumber,
             title,
@@ -29,5 +29,37 @@ export const createProblem = async (req, res) => {
     } catch (error) {
         console.error("Error in create problem controller: ", error.message);
         res.status(500).json({ success: false, message: "Internal Server Error"});
+    }
+}
+
+export const getAllProblems = async (req, res) => {
+    try {
+        const { difficulty, tags } = req.query;
+        let query = {};
+        if (difficulty) {
+            query.difficulty = difficulty;
+        }
+        if (tags) {
+            query.tags = tags;
+        }
+        const problems = await Problem.find(query);
+        res.status(200).json({success: true, problems});
+    } catch (error) {
+        console.error("Error in getAllProblems controller: ", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+export const getProblemByNumber = async (req, res) => {
+    try {
+        const problemNumber = req.params.problemNumber;
+        const problem = await Problem.findOne({problemNumber});
+        if (!problem) {
+            return res.status(400).json({ success: false, message: "Problem not found" });
+        }
+        res.status(200).json({ success: true, problem});
+    } catch (error) {
+        console.log("Error in getProblemByNumber controller:", error.message);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
     }
 }
--- backend/routes/problem.route.js
+++ backend/routes/problem.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createProblem } from "../controllers/problem.controller.js";
+import { createProblem, getAllProblems, getProblemByNumber } from "../controllers/problem.controller.js";
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createProblem);
+router.get("/", getAllProblems);
+router.get("/:problemNumber", getProblemByNumber);
 
 export default router;
